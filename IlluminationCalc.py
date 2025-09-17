e45ea03d69da8bf1af010b8a4829265e8911f1e9
Yiming Yang <yiming.yang@kaust.edu.sa>
2025-09-18 00:12:59 +0300

    Updated Annotation, Copyright

--- IlluminationCalc.py
+++ IlluminationCalc.py
@@ -1,3 +1,20 @@
+"""
+Copyright (C) 2025  YimingYang
+
+This program is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program.  If not, see <https://www.gnu.org/licenses/>.
+"""
+
 import tkinter as tk
 from tkinter import ttk
 import numpy as np
@@ -8,11 +25,10 @@ from LibCalc import CalculationType, Calculation
 from LibTkExtension import LabelEntryPair, LabelPicklistPair, TracePlotWindow
 
 
-
 # --------- BEGIN Create the Main Window ---------
 # Create the main window
 root = tk.Tk()
-root.title("Aperture Efficiency Calculator")
+root.title("Illumination Calculator (C) 2025  YimingYang")
 root.geometry("800x700")
 
 # Create the main frame
